1481bd8dc7c9ec37b57d3d5ca91c7945beda029b
ZiyuZhang6 <K23069561@kcl.ac.uk>
2025-03-02 01:31:56 +0000

    Update main.py

--- llm_research_assistant/main.py
+++ llm_research_assistant/main.py
@@ -18,5 +18,5 @@ def root():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", host="0.0.0.0", port=8000, reload=True
+        "llm_research_assistant.main:app", host="0.0.0.0", port=8000, reload=True
     )
