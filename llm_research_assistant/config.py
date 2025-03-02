87762288577d677b265eb08e2a8753ac82273a8e
ZiyuZhang6 <K23069561@kcl.ac.uk>
2025-03-02 15:17:04 +0000

    Update config.py

--- llm_research_assistant/config.py
+++ llm_research_assistant/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 class Settings(BaseSettings):
     # MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb+srv://...your-uri-here...")
-    MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb+srv://z1095395278:Zzy728371@cluster0.qc8qe.mongodb.net/?")
-    MONGODB_DB_NAME: str = os.getenv("MONGODB_DB_NAME", "Cluster0")
+    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    MONGODB_DB_NAME: str = os.getenv("MONGODB_DB_NAME")
     # MONGODB_DB_NAME: str = os.getenv("MONGODB_DB_NAME", "mydatabase")
 
     class Config:
